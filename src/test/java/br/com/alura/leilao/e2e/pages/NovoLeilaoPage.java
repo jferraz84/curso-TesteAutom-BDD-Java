83ff00cc43a429dcfaf9536a9e044ef634ade3ab
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-09-04 16:19:45 -0300

    Aula 6 - Tags e Contexto - Fim do Curso

--- src/test/java/br/com/alura/leilao/e2e/pages/NovoLeilaoPage.java
+++ src/test/java/br/com/alura/leilao/e2e/pages/NovoLeilaoPage.java
@@ -3,8 +3,6 @@ package br.com.alura.leilao.e2e.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NovoLeilaoPage {
 
